76416c7ca6d4fbc30db816a18e9a7ad782d63c56
Martin LE-ROY <mle-roy@e2r5p15.42.fr>
2014-06-23 14:43:31 +0200

    ok rajoute

--- make_broadcast.c
+++ make_broadcast.c
@@ -6,7 +6,7 @@
 /*   By: mle-roy <mle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/19 16:50:52 by mle-roy           #+#    #+#             */
-/*   Updated: 2014/06/19 16:53:29 by mle-roy          ###   ########.fr       */
+/*   Updated: 2014/06/23 14:43:04 by mle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ int				make_broadcast(t_action *act, t_player *pl, t_zaap *zaap)
 	}
 	if (zaap->gfx)
 		send_bc_gfx(pl->sock,act->buff, zaap->gfx);
+	add_player_buff(pl, "ok\n");
 	return (0);
 }
