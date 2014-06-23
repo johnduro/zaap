06f69a316a3f26052291a59f277cadb9236d943a
Martin LE-ROY <mle-roy@e2r5p15.42.fr>
2014-06-23 18:19:28 +0200

    norme

--- make_broadcast.c
+++ make_broadcast.c
@@ -6,7 +6,7 @@
 /*   By: mle-roy <mle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/19 16:50:52 by mle-roy           #+#    #+#             */
-/*   Updated: 2014/06/23 17:55:19 by mle-roy          ###   ########.fr       */
+/*   Updated: 2014/06/23 18:18:59 by mle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,11 +43,8 @@ static void		bc_to_pl(t_action *act, t_player *s, t_player *bwspl, t_zaap *z)
 int				make_broadcast(t_action *act, t_player *pl, t_zaap *zaap)
 {
 	t_team		*bwst;
-//	t_player	*bwspl;
-//	int			ret;
 
 	bwst = zaap->teams;
-//	ret = -1;
 	while (bwst)
 	{
 		if (bwst->first)
